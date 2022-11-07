cd358aed67e5a44ff079729faf39b4474272ff06
Rodrigo Ribeiro <ribeiro.rodrigodasilva@gmail.com>
2022-11-07 17:55:20 -0300

    First commit

---
+++ src/setup.js
@@ -0,0 +1,23 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors'
+import serve from 'koa-static'
+
+import { router } from './routes.js'
+import { errorMiddleware } from './middlewares/index.js'
+
+const app = new Koa()
+
+app.use(serve('./public'))
+app.use(bodyParser())
+app.use(cors())
+app.use(errorMiddleware)
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+const port = process.env.SERVER_PORT
+
+export function startServer() {
+    app.listen(port)
+}
